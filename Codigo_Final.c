c9ebedb38e8ffaa4bd4ef90ad8950a40c6da348e
Nahomy Castro <ncastrom21@gmail.com>
2023-09-19 18:07:01 -0600

    Se añadieron cambios para el proceso de pago con efecitvo.

--- Codigo_Final.c
+++ Codigo_Final.c
@@ -16,26 +16,21 @@
 #include <omp.h>
 #include <unistd.h>
 
+// Declaraciones previas
 
-// Creacion de función Compass
 void compass(int number);
-
-// Creacion de función Efectivo
 void efectivo(int number);
-
-// Creacion de función Estadísticas
 void estadisticas(int number);
-
-// Creacion de función Estadísticas
-void distribucionAutos(int number);
+void distribucionAutosCompass(int total_number);
+void distribucionAutosEfectivo(int total_number);
 
 // Variables para la cantidad de autos que pagarán con Compass y Efectivo.
 int numAutosConCompass;
 int numAutosEfectivo;
 
 // Variables para representar el tiempo de procesamiento de cada kiosco.
-double tiempoKioscoCompass;
-double tiempoKioscoEfectivo;
+double tiempoKiosconCompass;
+double tiempoKiosconEfectivo;
 
 // Variables para representar el tiempo total utilizado por cada kiosco.
 double tiempoTotalCompass;
@@ -48,54 +43,59 @@ double tiempoTotal;
 //Variables para realizar la simulación de paso de autos.
 
 int idAuto; // para identificar cada auto
-int tipoPago; // para representar si el auto paga con Compass o en efectivo
 double tiempoInicio; // para registrar el tiempo de inicio de paso por un kiosco
-
-int main(int argc, char const *argv[])
-{
-    printf("Bienvenido a la estacion: \n");
-    //Pide los datos
-    printf("Cuantos carros quieres para Compass: \n");
-    scanf("%d", &numAutosConCompass);
-    printf("Cuantos carros quieres para Efectivo:\n");
-    scanf("%d", &numAutosEfectivo);
-
-    //Hace la función con los carros Compass
-    distribucionAutos(numAutosConCompass);
-    
-    //Hace la función con los carros Efectivo
-
-
-
-    return 0;
-}
 
 
 void compass(int number) {
     for (int i = 0; i < number; i++) {
-        sleep(5);
-        printf("Gracias por usar Compass, buen viaje\n");
+        
+        sleep(2);
+        int localIdAuto;
+        int localNumAutosConCompass;
+        
+        #pragma omp atomic capture
+        localIdAuto = idAuto++;
+
+        #pragma omp atomic capture
+        localNumAutosConCompass = --numAutosConCompass;
+        
+        printf("\n\tEl auto numero %d esta usando Compass.", localIdAuto, localNumAutosConCompass);
+        printf("\n\tGracias por usar Compass, tenga buen viaje\n");
     }
 }
 
 void efectivo(int number) {
- //Parte Naho 
+    for (int i = 0; i < number; i++) {
+
+        sleep(2);
+        int localIdAuto;
+        int localNumAutosEfectivo;
+        
+        #pragma omp atomic capture
+        localIdAuto = idAuto++;
+
+        #pragma omp atomic capture
+        localNumAutosEfectivo = --numAutosEfectivo;
+        
+        printf("\n\tEl auto numero %d esta pagando con efectivo.", localIdAuto, localNumAutosEfectivo);
+        printf("\n\tGracias por pagar con efectivo, tenga buen viaje\n");
+    }
 }
+
+
 
 void estadisticas(int number) {
- //Parte Sofi 
+    // Parte de Sofi
 }
 
-//Repartir el total de autos en los hilos que tenemos
-void distribucionAutos(int total_number){
+void distribucionAutosCompass(int total_number) {
     #pragma omp parallel num_threads(3)
     {
         int thread_id = omp_get_thread_num();
         int carros_por_hilo = total_number / 3;
         int carros_extra = total_number % 3;
 
-        if (carros_extra != 0)
-        {
+        if (carros_extra != 0) {
             #pragma omp for
             for (int i = 0; i < 3; i++) {
                 if (thread_id == i) {
@@ -110,10 +110,44 @@ void distribucionAutos(int total_number){
                 }
             }
         } else compass(carros_por_hilo);
-        
-        
-
-        // Llama a compass una vez por hilo con la cantidad total a procesar
-        
     }
+}
+
+void distribucionAutosEfectivo(int total_number) {
+    #pragma omp parallel num_threads(3)
+    {
+        int thread_id = omp_get_thread_num();
+        int carros_por_hilo = total_number / 3;
+        int carros_extra = total_number % 3;
+
+        if (carros_extra != 0) {
+            #pragma omp for
+            for (int i = 0; i < 3; i++) {
+                if (thread_id == i) {
+                    if (i == 0) {
+                        efectivo(carros_por_hilo + 1);
+                    } else if (i == 1) {
+                        carros_extra -= 1;
+                        efectivo(carros_por_hilo + carros_extra);
+                    } else {
+                        efectivo(carros_por_hilo);
+                    }
+                }
+            }
+        } else efectivo(carros_por_hilo);
+    }
+}
+
+
+int main(int argc, char const *argv[]) {
+    printf("\nBienvenido a la estacion de pago de la VAS\n");
+    printf("\nPor favor indica cuantos carros desea pagar con Compass: ");
+    scanf("%d", &numAutosConCompass);
+    printf("\nPor favor indica cuantos carros deseas pagar con efectivo: ");
+    scanf("%d", &numAutosEfectivo);
+
+    distribucionAutosCompass(numAutosConCompass);
+    distribucionAutosEfectivo(numAutosEfectivo); 
+
+    return 0;
 }
