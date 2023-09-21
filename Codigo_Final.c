40a7a3fb9ec98ba4d5c2c3df18fbfdcacca66a16
Nahomy Castro <ncastrom21@gmail.com>
2023-09-20 19:13:16 -0600

    Se modificó la distribución de los autos para que se intercalaran entre compass y efectivo.

--- Codigo_Final.c
+++ Codigo_Final.c
@@ -1,15 +1,3 @@
-
-/*--------------------------------------------------------
-* UNIVERSIDAD DEL VALLE DE GUATEMALA
-* FACULTAD DE INGENIERÍA
-* DEPARTAMENTO DE CIENCIA DE LA COMPUTACIÓ
-* CC3086 Programación de Microprocesadores
-* 
-* Proyecto 02 - Fase 02 
-* Fecha: 21/09/2023
-* Descripción: Estadisticas de la Simulacion de la VAS
-* --------------------------------------------------------*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -17,133 +5,93 @@
 #include <unistd.h>
 
 // Declaraciones previas
-
 void compass(int number);
 void efectivo(int number);
 void estadisticas(double number);
-void distribucionAutosCompass(int total_number);
-void distribucionAutosEfectivo(int total_number);
+void distribucionAutos(int total_compass, int total_efectivo);
 
 // Variables para la cantidad de autos que pagarán con Compass y Efectivo.
-int numAutosConCompass;
-int numAutosEfectivo;
+int numAutosConCompass, numAutosEfectivo;
 
 // Variables para representar el tiempo de procesamiento de cada kiosco.
-double tiempoTotalCompass[3] = {0}; // Un arreglo para llevar el registro del tiempo de cada kiosco de Compass
-double tiempoTotalEfectivo[3] = {0}; // Un arreglo para llevar el registro del tiempo de cada kiosco de efectivo
+double tiempoTotalCompass[3] = {0}; 
+double tiempoTotalEfectivo[3] = {0}; 
 
-//Variables para realizar la simulación de paso de autos.
-
-int idAuto=0; // para identificar cada auto
-double tiempoInicio; // para registrar el tiempo de inicio de paso por un kiosco
-
+double tiempoInicio; 
+int nextAutoType = 0; 
+omp_lock_t lock; 
 
 void compass(int number) {
     for (int i = 0; i < number; i++) {
-        double start_time = omp_get_wtime(); // Registrar el tiempo de inicio
-        sleep(2);
-        int localIdAuto;
-        int localNumAutosConCompass;
-        
-        #pragma omp atomic capture
-        localIdAuto = idAuto++;
-
-        #pragma omp atomic capture
-        localNumAutosConCompass = --numAutosConCompass;
-        
-        printf("\n\tEl auto numero %d esta usando Compass.", localIdAuto, localNumAutosConCompass);
-        printf("\n\tGracias por usar Compass, tenga buen viaje\n");
-        double end_time = omp_get_wtime(); // Registrar el tiempo de finalización
-        int thread_id = omp_get_thread_num(); // Obtener el ID del thread
-        tiempoTotalCompass[thread_id] += (end_time - start_time); // Actualizar el tiempo total para este kiosco
+        double start_time = omp_get_wtime(); 
+        sleep(3);  
+        #pragma omp critical
+        {
+            printf("\n\tEl auto esta usando Compass.");
+            printf("\n\tGracias por usar Compass, tenga buen viaje\n");
+        }
+        double end_time = omp_get_wtime();  
+        int thread_id = omp_get_thread_num();
+        tiempoTotalCompass[thread_id] += (end_time - start_time);
     }
 }
 
 void efectivo(int number) {
     for (int i = 0; i < number; i++) {
-        double start_time = omp_get_wtime(); // Registrar el tiempo de inicio
-        sleep(2);
-        int localIdAuto;
-        int localNumAutosEfectivo;
-        
-        #pragma omp atomic capture
-        localIdAuto = idAuto++;
-
-        #pragma omp atomic capture
-        localNumAutosEfectivo = --numAutosEfectivo;
-        
-        printf("\n\tEl auto numero %d esta pagando con efectivo.", localIdAuto, localNumAutosEfectivo);
-        printf("\n\tGracias por pagar con efectivo, tenga buen viaje\n");
-        double end_time = omp_get_wtime(); // Registrar el tiempo de finalización
-        int thread_id = omp_get_thread_num(); // Obtener el ID del thread
-        tiempoTotalEfectivo[thread_id] += (end_time - start_time); // Actualizar el tiempo total para este kiosco
+        double start_time = omp_get_wtime(); 
+        sleep(3);      
+        #pragma omp critical
+        {
+            printf("\n\tEl auto esta pagando con efectivo."); 
+            printf("\n\tEsperando pago en efectivo...");
+            sleep(2);
+            printf("\n\tGracias por pagar con efectivo, tenga buen viaje\n");
+        }
+        double end_time = omp_get_wtime();  
+        int thread_id = omp_get_thread_num();
+        tiempoTotalEfectivo[thread_id] += (end_time - start_time);
     }
 }
-
-
 
 void estadisticas(double number) {
     printf("\n--- Estadisticas ---\n");
     for (int i = 0; i < 3; i++) {
         printf("Tiempo del kiosco Compass %d: %f segundos\n", i+1, tiempoTotalCompass[i]);
     }
-
     for (int i = 0; i < 3; i++) {
         printf("Tiempo del kiosco Efectivo %d: %f segundos\n", i+1, tiempoTotalEfectivo[i]);
     }
-    printf("Tiempo total de la estacion: %f segundos\n", number);
+    printf("\nTiempo total de la estacion: %f segundos\n\n", number);
 }
 
-void distribucionAutosCompass(int total_number) {
-    #pragma omp parallel num_threads(3)
-    {
-        int thread_id = omp_get_thread_num();
-        int carros_por_hilo = total_number / 3;
-        int carros_extra = total_number % 3;
+void distribucionAutos(int total_compass, int total_efectivo) {
+    int numKioscos = 3;
 
-        if (carros_extra != 0) {
-            #pragma omp for
-            for (int i = 0; i < 3; i++) {
-                if (thread_id == i) {
-                    if (i == 0) {
-                        compass(carros_por_hilo + 1);
-                    } else if (i == 1) {
-                        carros_extra -= 1;
-                        compass(carros_por_hilo + carros_extra);
-                    } else {
-                        compass(carros_por_hilo);
-                    }
+    int autosPorKioscoCompass = total_compass / numKioscos;
+    int autosExtraCompass = total_compass % numKioscos;
+
+    int autosPorKioscoEfectivo = total_efectivo / numKioscos;
+    int autosExtraEfectivo = total_efectivo % numKioscos;
+
+    #pragma omp parallel for
+    for (int i = 0; i < numKioscos; i++) {
+        for (int j = 0; j < (autosPorKioscoCompass + (i < autosExtraCompass ? 1 : 0) + autosPorKioscoEfectivo + (i < autosExtraEfectivo ? 1 : 0)); j++) {
+            if (j % 2 == 0) {
+                if (j/2 < autosPorKioscoEfectivo + (i < autosExtraEfectivo ? 1 : 0)) {
+                    efectivo(1);
+                } else {
+                    compass(1);
+                }
+            } else {
+                if (j/2 < autosPorKioscoCompass + (i < autosExtraCompass ? 1 : 0)) {
+                    compass(1);
+                } else {
+                    efectivo(1);
                 }
             }
-        } else compass(carros_por_hilo);
+        }
     }
 }
-
-void distribucionAutosEfectivo(int total_number) {
-    #pragma omp parallel num_threads(3)
-    {
-        int thread_id = omp_get_thread_num();
-        int carros_por_hilo = total_number / 3;
-        int carros_extra = total_number % 3;
-
-        if (carros_extra != 0) {
-            #pragma omp for
-            for (int i = 0; i < 3; i++) {
-                if (thread_id == i) {
-                    if (i == 0) {
-                        efectivo(carros_por_hilo + 1);
-                    } else if (i == 1) {
-                        carros_extra -= 1;
-                        efectivo(carros_por_hilo + carros_extra);
-                    } else {
-                        efectivo(carros_por_hilo);
-                    }
-                }
-            }
-        } else efectivo(carros_por_hilo);
-    }
-}
-
 
 int main(int argc, char const *argv[]) {
     printf("\nBienvenido a la estacion de pago de la VAS\n");
@@ -151,11 +99,13 @@ int main(int argc, char const *argv[]) {
     scanf("%d", &numAutosConCompass);
     printf("\nPor favor indica cuantos carros deseas pagar con efectivo: ");
     scanf("%d", &numAutosEfectivo);
-    double start_time = omp_get_wtime(); // Registrar el tiempo de inicio
-    distribucionAutosCompass(numAutosConCompass);
-    distribucionAutosEfectivo(numAutosEfectivo); 
-    double end_time = omp_get_wtime(); // Registrar el tiempo de finalización
-    estadisticas(end_time-start_time);  // Llamar a la función para imprimir las estadísticas
+
+    omp_init_lock(&lock);
+    double start_time = omp_get_wtime(); 
+    distribucionAutos(numAutosConCompass, numAutosEfectivo);
+    double end_time = omp_get_wtime(); 
+    estadisticas(end_time - start_time);  
+    omp_destroy_lock(&lock);
 
     return 0;
 }
